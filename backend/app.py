8caaf3d5362a3ad64dbf3088d99c825e0c3944cc
unknown <tavoywalls@gmail.com>
2022-12-14 18:06:06 -0500

    setup boiler plate

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,12 @@
-from flask import flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return 'HEY!'
+
+if __name__ == '__main__':
+    app.run()
