39dcab46b04b1bfa46bd88cc9c2ffdd9cb891c80
unknown <tavoywalls@gmail.com>
2022-12-14 18:37:38 -0500

    linked database

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,31 @@
+import requests
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pink1023Heart!@localhost:5432/tracker'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# if __name__ == '__main__':
+#     app.run()
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Event: {self.description}"
+
+    def __init__(self, description):
+        self.description = description
 
 @app.route('/')
 def hello():
     return 'HEY!'
-
-if __name__ == '__main__':
-    app.run()
