d8ee3833a9b70cef5789223bde0de1024b229b58
unknown <tavoywalls@gmail.com>
2022-12-14 23:46:48 -0500

    added delete button

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,15 @@
-
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pink1023Heart!@localhost:5432/tracker'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+CORS(app)
 migrate = Migrate(app, db)
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios"
-import {format} from "date-fns";
+// import { format } from "date-fns";
 
 import './App.css';
 
@@ -8,31 +8,70 @@ const baseUrl = "http://localhost:5000"
 
 function App() {
   const [description, setDescription] = useState("");
+  const [eventsList, setEventsList] = useState([]);
+
+  const fetchEvents = async () => {
+    const data = await axios.get(`${baseUrl}/events`)
+    const { events } = data.data
+    setEventsList(events);
+  }
 
   const handleChange = e => {
     setDescription(e.target.value);
   }
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log(description)
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${baseUrl}/events/${id}`)
+      const updatedList = eventsList.filter(event => event.id != id);
+      setEventsList(updatedList)
+    } catch (err) {
+      console.error(err.message)
+    }
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await axios.post(`${baseUrl}/events`, { description })
+      setEventsList([...eventsList, data.data]);
+      setDescription('');
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchEvents();
+  }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
+      <section>
         <form onSubmit={handleSubmit}>
           <label htmlFor='description'>Description</label>
           <input
-          onChange={handleChange}
-          type='text'
-          name='description'
-          id='description'
-          value={description}
+            onChange={handleChange}
+            type='text'
+            name='description'
+            id='description'
+            value={description}
           />
           <button type='submit'>Submit</button>
         </form>
-      </header>
+      </section>
+      <section>
+        <ul>
+          {eventsList.map(event => {
+            return (
+              <li style={{display : "flex"}} key={event.id}>
+                {event.description}
+                <button onClick={() => handleDelete(event.id)}>X</button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
